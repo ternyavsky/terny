bfe6eb2a0da5aa08feeb8c8f6eb0b16c7e1a5d06
ternyavsky <ilya.ternyavsky@yandex.ru>
2023-09-16 16:03:45 +0300

    add go

---
+++ backend/go.mod
@@ -0,0 +1,10 @@
+module backend
+
+go 1.21.0
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/rs/cors v1.10.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/graphql-go/graphql"
+	"github.com/mitchellh/mapstructure"
+	"github.com/rs/cors"
+)
+
+type User struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type jwtUserClaims struct {
+	User
+	jwt.StandardClaims
+}
+
+var jwtSecret []byte = []byte("golangbackend")
+
+var accounts []User = []User{
+	{
+		Id:       "1",
+		Username: "Admin",
+		Password: "Admin123",
+	},
+	{
+		Id:       "2",
+		Username: "Diman",
+		Password: "User123",
+	},
+}
+
+var accountType *graphql.Object = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Account",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"username": &graphql.Field{
+			Type: graphql.String,
+		},
+		"password": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
+
+func validateJwt(t string) (interface{}, error) {
+	if t == "" {
+		return nil, errors.New("Token not must be empty!")
+	}
+	token, _ := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Error")
+		}
+		return jwtSecret, nil
+	})
+	fmt.Println(token)
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		var decodedToken User
+		mapstructure.Decode(claims, &decodedToken)
+		fmt.Println(decodedToken)
+		return decodedToken, nil
+
+	} else {
+		return nil, errors.New("Invalid auth token!")
+	}
+}
+
+func CreateTokenEndpoint(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	claims := &jwtUserClaims{
+		user,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute).Unix(),
+		},
+	}
+
+	fmt.Println(user.Username)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenStr, err := token.SignedString(jwtSecret)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("content-type", "application/json")
+	w.Write([]byte(`{ "token": "` + tokenStr + `"}`))
+
+}
+
+func main() {
+	fmt.Println("Starting application at http://localhost:8000")
+	rootQuery := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"account": &graphql.Field{
+				Type: accountType,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					acc, err := validateJwt(p.Context.Value("token").(string))
+					if err != nil {
+						return nil, err
+
+					}
+					for _, account := range accounts {
+						if account.Username == acc.(User).Username {
+							return account, nil
+						}
+					}
+					return &User{}, nil
+				},
+			},
+		},
+	})
+
+	schema, _ := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
+	})
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := graphql.Do(graphql.Params{
+			Schema:        schema,
+			RequestString: r.URL.Query().Get("query"),
+			Context:       context.WithValue(context.Background(), "token", r.URL.Query().Get("token")),
+		})
+		json.NewEncoder(w).Encode(result)
+
+	})
+	mux.HandleFunc("/login", CreateTokenEndpoint)
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8000", handler)
+}
